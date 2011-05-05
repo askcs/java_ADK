7c0c148af110693bf0fdab8cdec0649e0553733a
Ludo Stellingwerff <ludo@almende.org>
2011-05-05 06:47:10 +0000

    Added various fields to message

--- src/com/askcs/ADK/AskatarMessage.java
+++ src/com/askcs/ADK/AskatarMessage.java
@@ -6,23 +6,43 @@ import com.askcs.webservices.ResourceDataResponse;
 
 public class AskatarMessage {
 
-	protected String messageUUID;
-	protected String message;
-	protected String type;
+	protected String messageUUID=null;
+	protected String message="";
+	protected String type="askMessage";
+	protected String sender="";
+	protected Boolean beenRead=false;
+	protected Integer prio=0;
 	
 	public AskatarMessage(String uuid, SessionHandler sh){
-		
+		ResourceDataResponse res;
+		Boolean error=false;
 		AskPortType askport = sh.getAskPort();
-		ResourceDataResponse res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "message", "TXT");
-		if(res.getError()==0){
-			message = res.getResult().getValue();
-		}
+		
+		res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "message", "TXT");
+		if(res.getError()==0) this.message = res.getResult().getValue(); else error=true;
+		res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "type", "TXT");
+		if(res.getError()==0) this.type = res.getResult().getValue(); else error=true;
+		res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "beenRead", "TXT");
+		if(res.getError()==0) this.beenRead=true;
+		res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "sender", "TXT");
+		if(res.getError()==0) this.sender = res.getResult().getValue(); else error=true;
+		res = askport.getResourceDataByTag(sh.getSessionId(), uuid, "prio", "TXT");
+		if(res.getError()==0) this.prio = Integer.getInteger(res.getResult().getValue()); else error=true;
+		
+		if (!error) this.messageUUID = uuid;
 	}
 	
-	public AskatarMessage(String message, String type){
-		
-		this.message=message;
-		this.type=type;
+	public AskatarMessage(String messageUUID,String message, String type, String sender, Integer prio){
+		this.messageUUID = messageUUID;
+		this.message = message;
+		this.type = type;
+		this.beenRead = false;
+		this.sender = sender;
+		this.prio = prio;
+	}
+
+	public String getMessageUUID() {
+		return messageUUID;
 	}
 
 	public String getMessage() {
@@ -32,4 +52,18 @@ public class AskatarMessage {
 	public String getType() {
 		return type;
 	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public Boolean getBeenRead() {
+		return beenRead;
+	}
+
+	public Integer getPrio() {
+		return prio;
+	}
+
+
 }
