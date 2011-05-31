d1ec3d5217b53e161c9b323455f2fc5c18a298c5
Erik Kemperman <ekemperman@ask-cs.com>
2011-05-31 15:29:54 +0000

    Add message digest / secure random

--- src/com/askcs/ADK/lib/SessionHandler.java
+++ src/com/askcs/ADK/lib/SessionHandler.java
@@ -1,7 +1,11 @@
 package com.askcs.ADK.lib;
 
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import com.askcs.webservices.Ask;
 import com.askcs.webservices.AskPortType;
@@ -27,11 +31,24 @@ public class SessionHandler {
 		}
 	}
 	
+	static public final String bytesToHex( byte[] src ) {
+		BigInteger i = new BigInteger( 1, src );
+	    String s = i.toString( 16 );
+	    if ( (s.length() & 1) != 0 ) {
+	        s = "0" + s;
+	    }
+	    return s;
+	}
+	
 	
 	private AskPortType fAskPort = null;
 	private String fEndPoint="";
 	private String fAuthKey="";
 	private String fSessionId="";
+	
+	private MessageDigest fDigest;
+	private SecureRandom fRandom;
+	
 		
 
 	private SessionHandler(String endpoint, String authKey){
@@ -47,6 +64,19 @@ public class SessionHandler {
 			fAuthKey = authKey;
 			startSession();
 		}
+		
+		try {
+			fDigest = MessageDigest.getInstance( "SHA-256" );
+		} catch ( NoSuchAlgorithmException x ) {
+			try {
+				fDigest = MessageDigest.getInstance( "MD5" );
+				System.out.println( "No SHA256 algo available, falling back on MD5(!)" );
+			} catch ( NoSuchAlgorithmException xx ) {
+				throw new RuntimeException( "Neither SHA256 nor MD5 algo available" );
+			}
+		}
+		
+		fRandom = new SecureRandom();
 	}
 	
 	protected boolean startSession(){
@@ -62,8 +92,20 @@ public class SessionHandler {
 	public AskPortType getAskPort(){
 		return fAskPort;
 	}
+	
 	public String getSessionId() {
 		return fSessionId;
+	}
+	
+	public byte[] getDigest( byte[] src ) {
+		fDigest.reset();
+		return fDigest.digest( src );
+	}
+	
+	public byte[] getRandom( int len ) {
+		byte[] b = new byte[ len ];
+		fRandom.nextBytes( b);
+		return b;
 	}
 }
 
